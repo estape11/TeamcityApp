5cb9c09dddd47ad666e643338e3168857e7dd3e7
Esteban AP <estape11@gmail.com>
2015-11-11 17:39:19 -0600

    Add files

---
+++ app/src/main/java/com/android/estebandroid/teamcityapp/mainScreenFragment.java
@@ -0,0 +1,22 @@
+package com.android.estebandroid.teamcityapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class mainScreenFragment extends Fragment {
+
+    public mainScreenFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    }
+}
